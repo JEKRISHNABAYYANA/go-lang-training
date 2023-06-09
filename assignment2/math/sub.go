94756a8f0d8950476356aecc5838546af0d0615f
BAYYANA JE KRISHNA <jb4385@nau.edu>
2023-06-09 08:34:51 -0700

    done with calculator program by using packages

---
+++ assignment2/math/sub.go
@@ -0,0 +1,11 @@
+package math
+
+import "fmt"
+
+//var Classcount int = 10
+//var Name string = "je krishna"
+
+func Subtraction(no1 int, no2 int) (int, int, int) {
+	fmt.Println(no1 - no2)
+	return no1, no2, no1 - no2
+}
